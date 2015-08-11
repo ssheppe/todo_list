97be8d65cbc85727b32718a1751e65f5fc587fcc
Samantha Sheppe <ssheppe@Samanthas-MacBook-Pro.local>
2015-08-11 13:09:23 -0400

    commit 3 routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next){
+	console.log(req.body);
+	res.send('success');
+})
+
 module.exports = router;
