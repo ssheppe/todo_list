de2242c8178cf7ba0d0f0e796f6f166a0df3474a
Samantha Sheppe <ssheppe@Samanthas-MacBook-Pro.local>
2015-08-11 16:05:41 -0400

    working code

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function (callback) {
+	console.log("successfully connected");
+});
+
+
+var taskSchema = mongoose.Schema({
+	description: String,
+	created_date: { type: Date, default: Date.now },
+	updated_date: { type: Date, default: Date.now },
+	completed: { type: Boolean, default: false }
+});
+
+var Task = mongoose.model('Task', taskSchema);
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	Task.find({}, function (err, taskList){
+		if (err) return console.error(err);
+		console.log(taskList);
+		res.render('index', { title: 'My fancy task list', taskList: taskList });
+	})
+  
+
 });
 
 router.post('/', function(req, res, next){
-	console.log(req.body);
-	res.send('success');
+	console.log("BODY", req.body);
+	var task = new Task({description: req.body.task});
+	task.save(function(err){
+		Task.find({}, function (err, taskList){
+			res.render('index', { title: 'My fancy task list', taskList: taskList});
+		});
+	});
+	
+	
 })
 
 module.exports = router;
